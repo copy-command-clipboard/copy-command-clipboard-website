f3fc463c5a16053d2a751efc8a27428515b2ff4a
yasgo <yasinburakkalkan@gmail.com>
2021-01-16 22:00:44 +0300

    SVG import ar-ge development.

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,23 @@
 <template>
   <div id="app">
     <Header />
+    <Logo />
   </div>
 </template>
 
 <script>
 import Header from "./components/layout/header";
+import Logo from "./assets/svg/logo.svg";
 
 export default {
   name: "App",
   components: {
     Header,
+    Logo
   },
 };
 </script>
 
 <style lang="scss">
-  @import "./style/app.scss";
+@import "./style/app.scss";
 </style>
