8ed604577de3216efc3534a69fb7a24a77f4c83e
yasgo <yasinburakkalkan@gmail.com>
2021-01-19 23:09:38 +0300

    Router & scss import fixed.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
-  <div id="app">
+  <div class="container">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Home</router-link>
+      <router-link to="/installation">Installation</router-link>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
+
+<style lang="scss">
+@import "./style/style.scss";
+</style>
