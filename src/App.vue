5a07075306ebb9821b8c6a4b1a5e093fef770459
yasgo <yasinburakkalkan@gmail.com>
2021-01-21 23:46:23 +0300

    Splash section development.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,22 @@
 <template>
-  <router-view />
+  <div class="root">
+    <div class="container">
+      <Header />
+    </div>
+    <router-view />
+  </div>
 </template>
+
+<script>
+import Header from "./components/layout/header";
+
+export default {
+  name: "Splash",
+  components: {
+    Header,
+  },
+};
+</script>
 
 <style lang="scss">
 @import "./style/style.scss";
--- src/components/splash/index.vue
+++ src/components/splash/index.vue
@@ -1,7 +1,6 @@
 <template>
   <section class="splash-section">
-    <div class="container h-100 d-flex flex-column justify-content-between">
-      <Header />
+    <div class="container h-100 d-flex flex-column justify-content-center">
       <article>
         <h1>Copy Command Clipboard</h1>
         <h2>
@@ -16,13 +15,11 @@
 </template>
 
 <script>
-import Header from "../layout/header";
 import Wave from "../../assets/svg/wave.svg";
 
 export default {
   name: "Splash",
   components: {
-    Header,
     Wave,
   },
 };
