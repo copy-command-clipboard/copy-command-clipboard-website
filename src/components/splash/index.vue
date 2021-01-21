057adb52e81c1a8393f03fda2a553b1bdab1a278
yasgo <yasinburakkalkan@gmail.com>
2021-01-21 23:42:35 +0300

    Splash section development.

--- src/components/splash/index.vue
+++ src/components/splash/index.vue
@@ -4,20 +4,26 @@
       <Header />
       <article>
         <h1>Copy Command Clipboard</h1>
-        <h2>Amazing loading animations for React. Waiting is less boring now.</h2>
+        <h2>
+          Amazing loading animations for React. Waiting is less boring now.
+        </h2>
       </article>
-      <div class="wave" :style="{backgroundImage: 'url(~@/assets/svg/wave.svg)'}"></div>
+    </div>
+    <div class="wave">
+      <Wave />
     </div>
   </section>
 </template>
 
 <script>
 import Header from "../layout/header";
+import Wave from "../../assets/svg/wave.svg";
 
 export default {
   name: "Splash",
   components: {
     Header,
+    Wave,
   },
 };
 </script>
