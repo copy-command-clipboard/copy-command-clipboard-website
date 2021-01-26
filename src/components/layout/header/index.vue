5473e7cdefe1df8bfd8faba61c5b87e1288e0165
yasgo <yasinburakkalkan@gmail.com>
2021-01-26 19:16:02 +0300

    Footer development.

--- src/components/layout/header/index.vue
+++ src/components/layout/header/index.vue
@@ -1,11 +1,15 @@
 <template>
   <header>
-    <router-link to="/">
-      <Logo />
-    </router-link>
-    <router-link to="/installation">
-      <Button color="silvertree" title="Documents" />
-    </router-link>
+    <div
+      class="container d-flex align-items-center align-md-items-end justify-content-between flex-column flex-md-row"
+    >
+      <router-link to="/">
+        <Logo />
+      </router-link>
+      <router-link to="/installation">
+        <Button color="silvertree" title="Documents" />
+      </router-link>
+    </div>
   </header>
 </template>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="pages">
+  <div class="home-content">
     <Splash />
     <Services />
   </div>
