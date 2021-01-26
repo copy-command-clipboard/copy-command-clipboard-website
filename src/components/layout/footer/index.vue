20257e825f81296299f9766ff3b7b080ff5d869b
yasgo <yasinburakkalkan@gmail.com>
2021-01-26 19:16:06 +0300

    Footer development.

--- src/components/layout/footer/index.vue
+++ src/components/layout/footer/index.vue
@@ -1,9 +1,7 @@
 <template>
   <footer>
     <div class="container">
-      <div
-        class="d-flex align-items-center align-md-items-end justify-content-between flex-column flex-md-row"
-      >
+      <div class="d-flex align-items-center align-md-items-end justify-content-between flex-column flex-md-row">
         <strong><a href="https://yasinkalkan.com/" target="blank">YASİN BURAK KALKAN</a> © 2021</strong>
         <Button color="silvertree" title="GET STARTED" />
       </div>
