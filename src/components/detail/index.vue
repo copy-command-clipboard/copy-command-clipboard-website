8340470bc578d1516463b76a6817c406ae9d983f
yasgo <yasinburakkalkan@gmail.com>
2021-01-28 20:27:59 +0300

    Detal page development.

--- src/components/detail/index.vue
+++ src/components/detail/index.vue
@@ -1,12 +1,12 @@
 <template>
   <section class="detail-section">
-    <h1>{{ title }}</h1>
-    <div class="row">
-      <div class="col-md-4">
-        sidebar
-      </div>
-      <div class="col-md-8">
-        <template></template>
+    <div class="container">
+      <h1>{{ title }}</h1>
+      <div class="row">
+        <div class="col-md-4">sidebar</div>
+        <div class="col-md-8">
+          <template></template>
+        </div>
       </div>
     </div>
   </section>
