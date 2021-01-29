1779776bf4c6026fd6f280400c95f71e271806aa
yasgo <yasinburakkalkan@gmail.com>
2021-01-29 22:56:13 +0300

    Sidebar development.

---
+++ src/components/sidebar/data.js
@@ -0,0 +1,50 @@
+export default [
+  {
+    id: "sidebar.gettingStarted.title",
+    title: "GETTING STARTED",
+    links: [
+      {
+        id: "sidebar.gettingStarted.installation.title",
+        title: "Installation",
+        url: "/installation"
+      },
+      {
+        id: "sidebar.gettingStarted.usage.title",
+        title: "Usage",
+        url: "/usage"
+      }
+    ]
+  },
+  {
+    id: "sidebar.api.title",
+    title: "API",
+    links: [
+      {
+        id: "sidebar.api.options.title",
+        title: "Options",
+        url: "/options"
+      },
+      {
+        id: "sidebar.api.classes.title",
+        title: "Animations",
+        url: "/animations"
+      }
+    ]
+  },
+  {
+    id: "sidebar.support.title",
+    title: "SUPPORT",
+    links: [
+      {
+        id: "sidebar.support.browser.title",
+        title: "Browser",
+        url: "/browser"
+      },
+      {
+        id: "sidebar.support.contact.title",
+        title: "Contact",
+        url: "/contact"
+      }
+    ]
+  }
+];
--- src/components/sidebar/index.vue
+++ src/components/sidebar/index.vue
@@ -1,13 +1,22 @@
 <template>
-  <aside></aside>
+  <aside>
+    <div class="col-md-4" v-for="item in data" :key="item.title">
+      {{ item.title }}
+    </div>
+  </aside>
 </template>
 
 <script>
-export default {
+import MenuData from "./data";
 
-}
+export default {
+  data() {
+    return {
+      data: MenuData,
+    };
+  },
+};
 </script>
 
 <style>
-
 </style>
