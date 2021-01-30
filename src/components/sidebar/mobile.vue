d85b2e782748174302dcfabd67f0c1fc8368ff0e
yasgo <yasinburakkalkan@gmail.com>
2021-01-30 22:17:39 +0300

    Sidebar mobile development.

---
+++ src/components/sidebar/mobile.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="select-container d-block d-sm-block d-md-none">
+    <select id="sidebar" name="sidebar" @change="onChange($event)">
+      <optgroup v-for="item in data" :key="item.id" :label="item.title">
+          <option v-for="subItem in item.links" :key="subItem.id" :value="subItem.url" :selected="current === subItem.url">{{subItem.title}}</option>
+        </optgroup>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    data: Array,
+  },
+  methods: {
+    onChange(event) {
+      console.log(event.target.value);
+      this.$router.push({ path: event.target.value })
+    },
+  },
+  data() {
+    return {
+      current: window.location.pathname,
+    };
+  },
+};
+</script>
+
+<style>
+</style>
