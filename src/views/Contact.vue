7a774cca9c3f386f53ec3513f0fa9a2e9cd741e8
yasgo <yasinburakkalkan@gmail.com>
2021-02-02 22:24:51 +0300

    Contact page development.

---
+++ src/views/Contact.vue
@@ -0,0 +1,16 @@
+<template>
+  <Detail title="Contact">
+    <span>Contact page.</span>
+  </Detail>
+</template>
+
+<script>
+import Detail from "../components/detail";
+
+export default {
+  name: "Contact",
+  components: {
+    Detail,
+  },
+};
+</script>
