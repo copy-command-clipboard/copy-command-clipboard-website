37f49def10f909b2d0f34a735480982b5cd3fe53
yasgo <yasinburakkalkan@gmail.com>
2021-02-02 22:21:44 +0300

    Options page development.

---
+++ src/views/Options.vue
@@ -0,0 +1,16 @@
+<template>
+  <Detail title="Options">
+    <span>Options page.</span>
+  </Detail>
+</template>
+
+<script>
+import Detail from "../components/detail";
+
+export default {
+  name: "Options",
+  components: {
+    Detail,
+  },
+};
+</script>
