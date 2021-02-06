9514f0bff21b2787faccfd65d6605c92fad97c4b
yasgo <yasinburakkalkan@gmail.com>
2021-02-06 20:33:28 +0300

    Installation page development.

--- src/views/Installation.vue
+++ src/views/Installation.vue
@@ -1,16 +1,25 @@
 <template>
   <Detail title="Installation">
-    <span>soner</span>
+    <article className="detail-item">
+      <h4>Github</h4>
+      <p className="mb-4">
+        You can clone repo from Github and free use in your project. Copy this
+        code and paste code in console.
+      </p>
+      <CopyCommand code="npm i copy-command-clipboard" />
+    </article>
   </Detail>
 </template>
 
 <script>
 import Detail from "../components/detail";
+import CopyCommand from "../components/copy-command";
 
 export default {
   name: "Installation",
   components: {
     Detail,
+    CopyCommand,
   },
 };
 </script>
