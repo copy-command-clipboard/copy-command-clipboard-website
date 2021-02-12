667d55ba2b29c245c2b9f034a8fd42ec0a75f0db
yasgo <yasinburakkalkan@gmail.com>
2021-02-12 22:45:18 +0300

    Usage page development.

--- src/views/Usage.vue
+++ src/views/Usage.vue
@@ -1,16 +1,27 @@
 <template>
   <Detail title="Usage">
-    <span>usage soner</span>
+    <article class="detail-item">
+      <h4>Import</h4>
+      <p class="mb-4">Firstly import copy-command-clipboard library.</p>
+      <CopyCommand code="import CopyCommandClipboard from 'copy-command-clipboard';" />
+    </article>
+    <article class="detail-item">
+      <h4>Usage</h4>
+      <p class="mb-4">In the simplest form, you can use it this way.</p>
+      <CopyCommand code="<CopyCommandClipboard code='npm i copy-command-clipboard' />" />
+    </article>
   </Detail>
 </template>
 
 <script>
 import Detail from "../components/detail";
+import CopyCommand from "../components/copy-command";
 
 export default {
   name: "Usage",
   components: {
     Detail,
+    CopyCommand,
   },
 };
 </script>
