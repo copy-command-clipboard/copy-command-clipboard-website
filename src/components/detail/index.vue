6cea7430d8bfaa10b7cd0b8bf6d3b829bdf7405f
yasgo <yasinburakkalkan@gmail.com>
2021-02-13 13:04:29 +0300

    Route changed and dynamic titles development completed.

--- src/components/detail/index.vue
+++ src/components/detail/index.vue
@@ -2,13 +2,13 @@
   <section class="detail-section">
     <div class="container">
       <h1 class="large silvertree">Copy Command Clipboard</h1>
-      <h2 class="small">GETTING STARTED</h2>
+      <h2 class="small">{{ titles.title }}</h2>
       <div class="row">
         <div class="col-md-4">
           <Sidebar />
         </div>
         <div class="col-md-8">
-          <h3>Installation</h3>
+          <h3>{{ titles.subTitle }}</h3>
           <slot />
         </div>
       </div>
@@ -22,10 +22,26 @@ import MenuData from "../sidebar/data";
 
 export default {
   created() {
-    console.log(window.location);
+    this.setTitles(window.location.pathname);
+  },
+  methods: {
+    setTitles(pathname) {
+      MenuData.forEach((item) => {
+        item.links.forEach((subItem) => {
+          if (subItem.url === pathname) {
+            this.titles.title = item.title;
+            this.titles.subTitle = subItem.title;
+          }
+        });
+      });
+    },
   },
   data() {
     return {
+      titles: {
+        title: "",
+        subTitle: "",
+      },
       data: MenuData,
     };
   },
