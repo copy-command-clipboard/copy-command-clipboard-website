cd23d596f07b39963a23c4878349ebe05ae22f96
yasgo <yasinburakkalkan@gmail.com>
2021-02-14 21:38:38 +0300

    Options page content development.

--- src/views/Options.vue
+++ src/views/Options.vue
@@ -1,6 +1,17 @@
 <template>
   <Detail>
-    <span>Options page.</span>
+    <article class="detail-item">
+      <h4>code</h4>
+      <p class="feature"><strong>Type:</strong> String</p>
+      <p class="feature"><strong>Default:</strong> ""</p>
+      <p>You can control visibility.</p>
+    </article>
+    <article class="detail-item">
+      <h4>isLarge</h4>
+      <p class="feature"><strong>Type:</strong> Boolean</p>
+      <p class="feature"><strong>Default:</strong> false</p>
+      <p>You can control visibility.</p>
+    </article>
   </Detail>
 </template>
 
