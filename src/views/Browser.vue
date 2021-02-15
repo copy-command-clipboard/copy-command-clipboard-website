3217d8be73f5738445f9eb8fe286792639579e7b
yasgo <yasinburakkalkan@gmail.com>
2021-02-15 12:48:07 +0300

    Browser page content development.

--- src/views/Browser.vue
+++ src/views/Browser.vue
@@ -2,22 +2,34 @@
   <Detail>
     <div class="row">
       <div class="col-lg-4 col-md-6">
-        <div class="browser-card"></div>
+        <div class="browser-card">
+          <Explorer />
+        </div>
       </div>
       <div class="col-lg-4 col-md-6">
-        <div class="browser-card"></div>
+        <div class="browser-card">
+          <Edge />
+        </div>
       </div>
       <div class="col-lg-4 col-md-6">
-        <div class="browser-card"></div>
+        <div class="browser-card">
+          <Firefox />
+        </div>
       </div>
       <div class="col-lg-4 col-md-6">
-        <div class="browser-card"></div>
+        <div class="browser-card">
+          <Opera />
+        </div>
       </div>
       <div class="col-lg-4 col-md-6">
-        <div class="browser-card"></div>
+        <div class="browser-card">
+          <Safari />
+        </div>
       </div>
       <div class="col-lg-4 col-md-6">
-        <div class="browser-card"></div>
+        <div class="browser-card">
+          <Chrome />
+        </div>
       </div>
     </div>
   </Detail>
@@ -25,11 +37,23 @@
 
 <script>
 import Detail from "../components/detail";
+import Explorer from "../assets/svg/explorer.svg";
+import Edge from "../assets/svg/edge.svg";
+import Firefox from "../assets/svg/firefox.svg";
+import Opera from "../assets/svg/opera.svg";
+import Safari from "../assets/svg/safari.svg";
+import Chrome from "../assets/svg/chrome.svg";
 
 export default {
   name: "Browser",
   components: {
     Detail,
+    Explorer,
+    Edge,
+    Firefox,
+    Opera,
+    Safari,
+    Chrome,
   },
 };
 </script>
