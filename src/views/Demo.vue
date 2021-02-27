c9375dbd86c532ed10f10e5d7145fb31ae80b4b1
yasgo <yasinburakkalkan@gmail.com>
2021-02-27 17:58:33 +0300

    Demo page completed.

--- src/views/Demo.vue
+++ src/views/Demo.vue
@@ -1,16 +1,31 @@
 <template>
   <Detail>
-    Demo
+    <p class="mb-2">Write anything in input:</p>
+    <input v-model="commandValue" type="text" @keyup="onKeyup($event)" class="mb-5" />
+    <p class="mb-2">And copy-paste:</p>
+    <CopyCommand :code="commandValue" />
   </Detail>
 </template>
 
 <script>
 import Detail from "../components/detail";
+import CopyCommand from "../components/copy-command";
 
 export default {
   name: "Demo",
+  methods: {
+    onKeyup(event) {
+      this.commandValue = event.target.value;
+    },
+  },
+  data() {
+    return {
+      commandValue: "npm i copy-command-clipboard",
+    };
+  },
   components: {
     Detail,
+    CopyCommand,
   },
 };
 </script>
