c37750066a8d1f61691d6f2609424bfbdc3d641e
yasgo <yasinburakkalkan@gmail.com>
2021-02-27 15:30:58 +0300

    Contact page development.

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,16 +1,19 @@
 <template>
   <Detail>
-    <span>Contact page.</span>
+    <p>You can send mail for questions and problems. I am waiting every time your feedback. Please help me for better development my project. You can fast reach me in email.</p>
+    <a href="mailto:yasinburakkalkan@gmail.com" class="d-flex align-items-center mt-3"><GmailLogo class="mr-3" /> yasinburakkalkan@gmail.com</a>
   </Detail>
 </template>
 
 <script>
 import Detail from "../components/detail";
+import GmailLogo from "../components/logo/gmail";
 
 export default {
   name: "Contact",
   components: {
     Detail,
+    GmailLogo
   },
 };
 </script>
