f63785e3498a14b7e4dd706159111a4b28f92dd6
yasgo <yasinburakkalkan@gmail.com>
2021-02-28 14:30:06 +0300

    Development environment section development.

---
+++ src/components/development-environments/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="development-environments-section">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4 d-flex align-items-center justify-content-center">
+          <font-awesome-icon :icon="['fab', 'github']" class="icon alt"/>
+        </div>
+        <div class="col-md-4 d-flex align-items-center justify-content-center">
+          <i class="icon-github"></i>
+        </div>
+        <div class="col-md-4 d-flex align-items-center justify-content-center">
+          <i class="icon-npm"></i>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DevelopmentEnvironments",
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,18 +2,21 @@
   <div class="home-content">
     <Splash />
     <Services />
+    <DevelopmentEnvironments />
   </div>
 </template>
 
 <script>
 import Splash from "../components/splash";
 import Services from "../components/services";
+import DevelopmentEnvironments from "../components/development-environments";
 
 export default {
   name: "Home",
   components: {
     Splash,
     Services,
+    DevelopmentEnvironments,
   },
 };
 </script>
